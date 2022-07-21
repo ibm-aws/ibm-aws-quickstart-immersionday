59ef1d319616891295ffd5fcc40d76438a479f0d
Sunil Gajula <sunilgajula@MacBook-Pro.local>
2022-07-21 18:32:32 +0530

    Removing C9OwnerArn as its no longer required

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -24,6 +24,17 @@ function create_users() {
 
     echo "Users created successfully."
     echo "Password for both user$ram and datascientist$ram is: password"
+
+    export user_id=$(curl -k -s "https://$route/usermgmt/v1/user/user$ram" -H "authorization: Bearer $token" -H "Content-Type: application/json" | jq -r .uid)
+
+    curl -s -k -X GET "https://$route/zen-data/v3/service_instances?fetch_all_instances=true" -H "Authorization: Bearer $token" | jq -r '.service_instances[] | .id' > /tmp/id.txt
+
+    while read line
+    do
+        echo "Granting Permissions to $line Instance"
+    	curl -k -X POST "https://$route/zen-data/v2/serviceInstance/users/" -H "authorization: Bearer $token" -H "Content-Type: application/json" -d "{ \"serviceInstanceID\": \"$line\", \"users\": [{ \"role\": \"Admin\", \"uid\": \"$user_id\", \"username\": \"user$ram\" }] }"
+    done < /tmp/id.txt
+
   else
     echo "Error in CP4D Token extraction"
   fi

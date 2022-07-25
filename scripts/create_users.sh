1a25f182fe0f04f83288e9aa5998e9a187eeb701
Sunil Gajula <sunilgajula@MacBook-Pro.local>
2022-07-25 14:17:14 +0530

    Providing access to instances for users

--- scripts/create_users.sh
+++ scripts/create_users.sh
@@ -20,10 +20,21 @@ function create_users() {
     export ram=$RANDOM
     curl -k -X POST -H "Authorization: Bearer $token" -H "cache-control: no-cache" -d "{\"user_name\":\"user$ram\",\"password\":\"password\",\"displayName\":\"User$ram\",\"permissions\":[\"administrator\",\"can_provision\"],\"user_roles\":[\"zen_administrator_role\"],\"email\":\"user$ram@user.com\"}" "https://$route/icp4d-api/v1/users" -H "Content-Type: application/json"
 
-    curl -k -X POST -H "Authorization: Bearer $token" -H "cache-control: no-cache" -d "{\"user_name\":\"datascientist$ram\",\"password\":\"password\",\"displayName\":\"datascientist$ram\",\"permissions\":[\"access_catalog\",\"create_project\",\"create_space\"],\"user_roles\":[\"wkc_data_scientist_role\"],\"email\":\"datascientist@test.com\"}" "https://$route/icp4d-api/v1/users" -H "Content-Type: application/json"
+    curl -k -X POST -H "Authorization: Bearer $token" -H "cache-control: no-cache" -d "{\"user_name\":\"datascientist$ram\",\"password\":\"password\",\"displayName\":\"datascientist$ram\",\"permissions\":[\"sign_in_only\"],\"user_roles\":[\"zen_user_role\"],\"email\":\"datascientist@test.com\"}" "https://$route/icp4d-api/v1/users" -H "Content-Type: application/json"
 
     echo "Users created successfully."
     echo "Password for both user$ram and datascientist$ram is: password"
+
+    export user_id=$(curl -k -s "https://$route/usermgmt/v1/user/user$ram" -H "authorization: Bearer $token" -H "Content-Type: application/json" | jq -r .uid)
+
+    curl -s -k -X GET "https://$route/zen-data/v3/service_instances?fetch_all_instances=true" -H "Authorization: Bearer $token" | jq -r '.service_instances[] | .id' > /tmp/id.txt
+
+    while read line
+    do
+        echo "Granting Permissions to $line Instance"
+    	curl --silent --output /dev/null --show-error --fail -k -X POST "https://$route/zen-data/v2/serviceInstance/users/" -H "authorization: Bearer $token" -H "Content-Type: application/json" -d "{ \"serviceInstanceID\": \"$line\", \"users\": [{ \"role\": \"Admin\", \"uid\": \"$user_id\", \"username\": \"user$ram\" }] }"
+    done < /tmp/id.txt
+
   else
     echo "Error in CP4D Token extraction"
   fi

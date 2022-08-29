a2963dfbd1b29ebca83d042f5e3a05d0fa8a64bd
barochiarg <Rishitkumar.Barochia@ibm.com>
2022-08-29 16:09:05 +0530

    add predictionapp deployemnt

---
+++ templates/config/predictionappdeployment.sh
@@ -0,0 +1,56 @@
+
+## validate argument
+if [ -z $1 ]; then
+    echo "EKS clustername must be supplied as argument-1"
+    exit 1;
+fi
+
+export CLUSTERNAME=$1
+export APPFOLDERNAME=./"predictionapp"
+export APPNAMESPACE="immersiondaytest"
+export LBPATTERN="risk-index"
+export R53PARENTDOMAIN="ibmworkshops.com"
+export R53RECORDNAME="test2-prediction-app.ibmworkshops.com"
+export R53TTL=300
+export R53REQUESTFILENAME="r53request.json"
+export R53APIPUBLICURL="https://e1s28hehsd.execute-api.us-east-2.amazonaws.com/dev/route53/records/cname/add"
+
+## create prdictionapp folder
+rm -rf $APPFOLDERNAME
+mkdir -p $APPFOLDERNAME
+cd $APPFOLDERNAME
+
+## checkout repo
+wget https://github.com/ibm-aws/immersion-day-lab4-app/archive/refs/heads/main.zip
+unzip ./main.zip
+
+## login to EKS cluster
+aws eks --region us-east-2 update-kubeconfig --name $CLUSTERNAME
+
+## verify EKS login
+if [ $? -ne 0 ];then
+        echo "unable to login to cluster. please check clustername $CLUSTERNAME"
+        exit 1;
+fi
+
+## deploy application
+kubectl apply -f immersion-day-lab4-app-main/deploy.yaml -n $APPNAMESPACE
+
+## Get LB url
+export LBURL=$(kubectl get svc -n $APPNAMESPACE | grep $LBPATTERN | awk '{print $4}')
+echo $LBURL
+
+## prepare request json file
+export R53_REQUESTJSON=$(jq -n \
+                  --arg parentdomain "$R53PARENTDOMAIN" \
+                  --arg ttl "$R53TTL" \
+                  --arg recordName "$R53RECORDNAME" \
+                  --arg value "$LBURL" \
+                   '$ARGS.named')
+
+echo $R53_REQUESTJSON
+rm -f ./$R53REQUESTFILENAME
+echo "$R53_REQUESTJSON" > $R53REQUESTFILENAME
+
+## invoke API url 
+curl -X POST -H "Content-Type: application/json" -d "@$R53REQUESTFILENAME" $R53APIPUBLICURL
